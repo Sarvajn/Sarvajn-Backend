576d094c8a31c39fa477442ba4165e32fa9f1d84
Anurag Bhatt <anuragbhatt1805@gmail.com>
2026-02-10 03:03:54 +0530

    feat: Add Winston-based logging and refactor application startup with database connection checks.

--- api/auth/src/app.ts
+++ api/auth/src/app.ts
@@ -1,16 +1,32 @@
-import { prisma } from "./libs/prisma.lib.js";
+// import { prisma } from "./libs/prisma.lib.js";
 
-async function main() {
-  const allUsers = await prisma.user.findMany();
-  console.log("All users:", JSON.stringify(allUsers, null, 2));
-}
+// async function main() {
+//   const allUsers = await prisma.user.findMany();
+//   console.log("All users:", JSON.stringify(allUsers, null, 2));
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
+
+import express, { type Express } from "express";
+
+import logger from "./utils/logger.util.js";
+
+const log = logger({ class: "app" });
+const app: Express = express();
+app.disable("x-powered-by");
+
+// const port = process.env.PORT || 1000;
+
+// app.listen(port, () => {
+//   log.info(`Sarvajn Auth Server is running on port ${port}`);
+// });
+
+export default app;
---
+++ api/auth/src/utils/logger.util.ts
@@ -0,0 +1,32 @@
+import winston from "winston";
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+// Custom log format
+const logFormat = printf(
+  ({ level, message, timestamp, service, file, ...metadata }) => {
+    const fileOrClass = file || metadata.class;
+    const prefix = fileOrClass ? ` [${fileOrClass}]` : "";
+    return `[${timestamp}] [${service}]${prefix} ${message}`;
+  },
+);
+
+const baseLogger = winston.createLogger({
+  level: "info",
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
+  defaultMeta: { service: "sarvajn-auth" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console({
+      format: combine(
+        colorize(),
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        logFormat,
+      ),
+    }),
+  ],
+});
+
+export default (meta: { file?: string; class?: string }) =>
+  baseLogger.child(meta);
